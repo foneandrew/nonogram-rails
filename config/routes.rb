3d74c005d1e41a1182bb4e9abaf0d6b962a25984
Andy Fone <fone.andrew@gmail.com>
2014-12-07 14:17:57 +1300

    modified User to include a display name

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,15 @@
 Rails.application.routes.draw do
 
-  devise_for :users
+  root 'games#index'
+
+  devise_for :users, :controllers => { registrations: 'registrations' }
 
   resources :games, only: [:index, :show, :create]
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'games#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
