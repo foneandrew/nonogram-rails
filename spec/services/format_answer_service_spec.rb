2ad84428647a656ba7bfee7ef2bac2bc2d9613f8
Andy Fone <fone.andrew@gmail.com>
2014-12-16 13:37:02 +1300

    added tests for format answer service

--- spec/services/format_answer_service_spec.rb
+++ spec/services/format_answer_service_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe FormatAnswerService, :type => :service do
-  pending "add some examples to (or delete) #{__FILE__}"
+  fixtures :nonograms
+
+  describe '#call' do
+    let(:cells) { 
+      { "0" => { "0" => "1", "1" => "1", "2" => "1" },
+        "1" => { "0" => "1", "1" => "1" },
+        "2" => { "0" => "1", "2" => "1" },
+        "3" => { "0" => "1", "1" => "1", "3" => "1" },
+        "4" => { "0" => "1", "1" => "1", "2" => "1" }
+      } 
+    }
+    let(:solution) { nonograms(:nonogram_size_5).solution }
+    #nonogram stored in nonogram_size_5:
+    #1 1 1 0 0
+    #1 1 0 0 0
+    #1 0 1 0 0
+    #1 1 0 1 0
+    #1 1 1 0 0
+
+    it 'will convert a hash of cells to a string' do
+      format_answer = FormatAnswerService.new(cells: cells, size: 5)
+      expect(format_answer.call).to eq solution
+    end
+  end
 end
