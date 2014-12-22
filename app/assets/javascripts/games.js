a2321b606625901686954df01fc7aab70ae41a7f
Andy Fone <fone.andrew@gmail.com>
2014-12-23 11:14:04 +1300

    pages now refresh on changesgad Just uses hyper basic page reloading for now

--- app/assets/javascripts/games.js
+++ app/assets/javascripts/games.js
@@ -1,22 +1,42 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+var state
+
 $(function (){
   //this happens on page load
-  if ($('#game').length > 0) {
+  if ($('#game').length) {
     console.log("On a game page");
-    setTimeout(thang, 2000);
-  } else if (($('#games').length > 0)) {
+    poll(2000);
+  } else if (($('#games').length)) {
     console.log("On the index page")
   } else {
     console.log("Not on a game page")
   }
 });
 
-function thang() {
-  // window.alert("ummm...");
+var poll = function(timeout){
   var id = $('#game').attr('data-game-id');
 
-  $.getJSON(Routes.game_path(id), function(json) {
-    console.log(json);
+  var promise = $.getJSON(Routes.game_path(id));
+  
+  promise.done(function(json) {
+    if (state) {
+      console.log("original: " + state);
+      console.log("new: " + JSON.stringify(json.game));
+      var compare = state.localeCompare(JSON.stringify(json.game));
+      console.log("compared: " + compare);
+      if (compare) {
+        console.log("CHANGE!!")
+        window.location.reload(true);
+      }
+    }
+
+    state = JSON.stringify(json.game);
   });
-}
+
+  promise.always(function() {
+    setTimeout(function() {
+      poll(timeout)
+    }, timeout);
+  });
+};
