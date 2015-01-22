264a4c4a430c2b26d2d6365d7b2b3bf187947d69
Andy Fone <fone.andrew@gmail.com>
2015-01-23 11:42:15 +1300

    removed status from serializer

--- app/serializers/game_serializer.rb
+++ app/serializers/game_serializer.rb
@@ -1,22 +1,9 @@
 class GameSerializer < ActiveModel::Serializer
-  attributes :id, :started?, :completed?, :player_count, :status
+  attributes :id, :started?, :completed?, :player_count
 
   private
 
-  # TODO remove status shite
-
   def player_count
     object.players.count
-  end
-
-  def status
-    status_string = ""
-    case
-    when object.completed? then status_string += 'f'
-    when object.started? then status_string += 's'
-    else status_string += 'n'
-    end
-
-    status_string += player_count.to_s
   end
 end
